211eaf979f2ed58ac682703f86ad6c36534b715b
Yash Bele <krupabele47@gmail.com>
2025-07-18 19:51:13 +0530

    init

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -137,6 +137,7 @@ const Dashboard: React.FC<DashboardProps> = ({
               <button className="text-gray-600 hover:text-gray-900 px-3 py-2 text-sm font-medium">
                 Settings
               </button>
+              <h1>env:{API_URL}</h1>
             </div>
           </div>
         </div>
